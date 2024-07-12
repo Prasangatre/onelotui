d93bfb8cb0fe9517d4690deba3f1f11ec4d0183b
Prasang <prasangatre260@gmail.com>
2024-07-12 16:59:11 +0530

    responsiveness and hero section enhancment

--- src/components/hero/page.tsx
+++ src/components/hero/page.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 const HeroSection = () => {
   return (
-    <div>
+    <div className="mt-5">
       <div className="w-full flex items-center flex-col justify-center">
-        <div className="bg-red-100 flex flex-col w-[70%] justify-center items-center gap-4">
-          <div className="text-7xl font-bold text-center text-[#000000]">
+        <div className=" flex flex-col w-[100%] text-justify p-2  md:w-[60%] md:text-center justify-center items-center gap-4">
+          <div className="text-6xl font-bold text-center text-[#000000]">
             Grow your dealership with car loans and digital tools for dealers
           </div>
-          <div className="text-xl text-center font-medium text-[#000000]">
+          <div className="text-xl text-center font-medium text-[#6b7280]">
             We enable dealers to buy more cars through our inventory loan
             solutions and sell cars more efficiently through our Dealership
             Management System with its inventory management, marketing solutions
@@ -20,15 +20,20 @@ const HeroSection = () => {
             <CustomButton textToDisplay={"Inquire now"} />
           </div>
         </div>
-        <div className="bg-green-200 w-[90%] flex items-center justify-center flex-col">
+        <div className=" w-[90%] flex items-center justify-center flex-col gap-2">
           <div>
-            <Image src={"/cars.jpg"} height={1000} width={1500} />
+            <Image
+              alt="carImage"
+              src={"/cars.jpg"}
+              height={1000}
+              width={1500}
+            />
           </div>
           <div className="text-[#6b7280] font-medium text-base">
             Regulated by
           </div>
           <div>
-            <Image src={"/sec.jpg"} height={100} width={100} />
+            <Image alt="" src={"/sec.jpg"} height={100} width={100} />
           </div>
         </div>
       </div>
