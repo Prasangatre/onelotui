3567b29627ac7bfd8b056c7da0b2f2b506d496bc
Prasang <prasangatre260@gmail.com>
2024-07-12 18:01:29 +0530

    responsiveness for about section

--- src/components/about/page.tsx
+++ src/components/about/page.tsx
@@ -1,21 +1,37 @@
+import Image from "next/image";
 import React from "react";
+import { FaArrowRight } from "react-icons/fa";
 
 const About = () => {
   return (
-    <div className="flex gap-8 items-start bg-lime-50 px-8 py-6">
+    <div className="flex flex-col px-2 md:flex-row gap-8 items-start  md:px-20 py-6 w-full">
       <div className="flex flex-col gap-3">
         <div className="font-bold text-[#000] text-xl">
           Everything you need to grow your business
         </div>
-        <div className="font-bold text-[#000] text-xl">
+        <div className="font-medium text-[#6b7280] text-lg text-justify">
           OneLot is your trusted lending and financing partner, exclusively
           tailored for used car dealership. We also offer digital tools for
           streamlined operations and growth of your dealership.
         </div>
+        <div className="flex items-center gap-2">
+          <div className="text-[#57099C]">Learn more</div>
+          <div>
+            <FaArrowRight color="#57099C" fontSize={"0.8rem"} />
+          </div>
+        </div>
       </div>
-      <div className="flex flex-col gap-4">
-        <div className="flex items-start gap-1">
-          <div> iI</div>
+      <div className="flex flex-col  gap-4">
+        <div className="flex items-start gap-3">
+          <div>
+            <Image
+              loading="lazy"
+              alt="speaker"
+              src={"/dollar.svg"}
+              width={"40"}
+              height={"30"}
+            />
+          </div>
           <div className="flex flex-col gap-2">
             <div className="text-base font-semibold text-[#000000]">
               Dealer Inventory Loans
@@ -31,12 +47,23 @@ const About = () => {
               <div className="text-md font-medium text-[#7e3af2]">
                 Learn more
               </div>
-              <div>iI</div>
+              <div>
+                <FaArrowRight color="#57099C" fontSize={"0.8rem"} />
+              </div>
             </div>
           </div>
         </div>
-        <div className="flex items-start gap-1">
-          <div> iI</div>
+        <div className="flex  items-start gap-3">
+          <div>
+            {" "}
+            <Image
+              loading="lazy"
+              alt="mic"
+              src={"/mic.png"}
+              width={"40"}
+              height={"30"}
+            />
+          </div>
           <div className="flex flex-col gap-2">
             <div className="text-base font-semibold text-[#000000]">
               Dealership Management System
@@ -52,7 +79,9 @@ const About = () => {
               <div className="text-md font-medium text-[#7e3af2]">
                 Learn more
               </div>
-              <div>iI</div>
+              <div>
+                <FaArrowRight color="#57099C" fontSize={"0.8rem"} />
+              </div>
             </div>
           </div>
         </div>
